022f5ba332aba5d825409ad51c03610ecb93b9a0
oportusc <oportus.c@gmail.com>
2025-07-03 14:27:24 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:5173', // Cambia esto si tu frontend está en otro puerto o dominio
+    credentials: true,
+  });
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap(); 
